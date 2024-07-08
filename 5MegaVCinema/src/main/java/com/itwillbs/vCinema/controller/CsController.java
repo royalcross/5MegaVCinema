5d44cd6d2a72f63f267447dbf7368481b09c260e
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-07-08 16:54:32 +0900

    수안

--- 5MegaVCinema/src/main/java/com/itwillbs/vCinema/controller/CsController.java
+++ 5MegaVCinema/src/main/java/com/itwillbs/vCinema/controller/CsController.java
@@ -1,5 +1,7 @@
 package com.itwillbs.vCinema.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +18,7 @@ public class CsController {
 	private CsService service;
 	
 	@GetMapping("Cs1")
-	public String cs1() {
+	public String cs1(HttpSession session, Model model) {
 		return "cs/cs1";
 	}
 
