d5b12e0262cf7065919e5ce1d96ff4659c28e308
KSY <ksysm427@naver.com>
2024-07-22 18:18:35 +0900

    김성윤 - 0722(스토어 상품 이미지 등록 진행 중)

--- 5MegaVCinema/src/main/java/com/itwillbs/vCinema/controller/AdminStoreController.java
+++ 5MegaVCinema/src/main/java/com/itwillbs/vCinema/controller/AdminStoreController.java
@@ -1,6 +1,13 @@
 package com.itwillbs.vCinema.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.itwillbs.vCinema.service.AdminStoreService;
 import com.itwillbs.vCinema.vo.ItemVO;
@@ -15,9 +23,11 @@ import com.itwillbs.vCinema.vo.PageInfo;
 
 @Controller
 public class AdminStoreController {
-	
 	@Autowired
 	private AdminStoreService service;
+	
+	// 가상의 경로명 저장(이클립스 프로젝트 상의 경로)
+	private String uploadPath = "/resources/upload";
 	
 	@GetMapping("AdminStore")
 	public String adminStoreHome(@RequestParam(defaultValue = "1") int pageNum, Model model, 
@@ -91,19 +101,59 @@ public class AdminStoreController {
 	// =================================================================================================
 	// 아이템 등록
 	@PostMapping("AdminItemRegist")
-	public String adminItemRegist(ItemVO item, Model model, String item_type) {
+	public String adminItemRegist(ItemVO item, Model model, String item_type, HttpSession session) {
 //		System.out.println(item);
+		// [ 파일 업로드 경로 관리 ]
+		String realPath = session.getServletContext().getRealPath(uploadPath);
+		String subDir = "item";
+		realPath += "/" + subDir;
+		try {
+			// 해당 디렉토리를 실제 경로에 생성(단, 존재하지 않을 경우에만 자동 생성)
+			// java.nio.file.Paths 클래스의 get() 메서드 호출하여
+			// 실제 업로드 경로를 관리하는 java.nio.file.Path 객체 리턴받기
+			Path path = Paths.get(realPath); // 파라미터로 실제 업로드 경로 전달
+			
+			// Files 클래스의 createDirectories() 메서드 호출하여 실제 경로 생성
+			// => 이 때, 경로 상에서 생성되지 않은 모든 디렉토리 생성
+			//    만약, 최종 서브디렉토리 1개만 생성 시 createDirectory() 메서드도 사용 가능
+			Files.createDirectories(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// [ 업로드 되는 실제 파일 처리 ]
+		// 실제 파일은 ItemVO 객체의 MultipartFile 타입 객체(멤버변수 image)가 관리함
+//		MultipartFile image = item.getImage();
+		// BoardVO 객체에 서브디렉토리명과 함께 파일명 저장
+//		String imageName = image.getOriginalFilename();
+//		item.setItem_image(subDir + "/" + imageName);
+		
+		// AdminStoreService - adminItemRegist() 메서드 호출하여 상품 등록 작업 요청
+		// => 파라미터 : ItemVO 객체   리턴타입 : int(insertCount)
 		// AdminStoreService - AdminStoreRegist(item) -- int;
 		int insertCount = service.adminItemRegist(item);
 		
-		if(insertCount > 0 ) {
+		// 상품 등록 작업 요청 결과 판별
+		if(insertCount > 0 ) { // 성공
+//			try {
+//				// 업로드 파일들은 MultipartFile 객체에 의해 임시 저장공간에 저장되어 있으며
+//				// 상품 등록 성공 시 임시 저장공간 -> 실제 디렉토리로 이동 작업 필요
+//				// => MultipartFile 객체의 transferTo() 메서드 호출하여 실제 위치로 이동 처리
+//				//    (파라미터 : java.io.File 타입 객체 전달)
+//				// File 객체 생성 시 생성자에 업로드 경로명과 파일명 전달
+//				image.transferTo(new File(realPath, imageName));
+//			} catch (IllegalStateException e) {
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+			
+			// 성공 시 "성공적으로 처리되었습니다." 메세지 출력 및 "AdminStore?pageNum=1" 서블릿 주소 전달(success.jsp)
 			model.addAttribute("msg", "성공적으로 처리되었습니다.");
 			model.addAttribute("targetURL", "AdminStore?pageNum=1");
-			
 			return "result/success";
-		} else {
+		} else { // 실패
+			// 실패 시 "아이템 등록에 실패했습니다." 메세지 출력 및 이전페이지 처리(fail.jsp)
 			model.addAttribute("msg", "아이템 등록에 실패했습니다.");
-			
 			return "result/fail";
 		}
 		
